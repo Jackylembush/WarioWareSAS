48fa976f8eb5f3b08f0cb058bc32bfc357c37e43
SamuelM <miquelspro@gmail.com>
2020-12-04 16:14:19 +0100

    50m!_Base

---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioSAS/50m!/First50mTest/TestScript/Running.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Running : MonoBehaviour
+{
+    public Transform PlatformObj;
+    private Vector3 nextTileSpawn;
+
+    void Start()
+    {
+        nextTileSpawn.z = 5;
+        StartCoroutine(spawnTile());
+    }
+
+    void Update()
+    {
+        
+    }
+    IEnumerator spawnTile()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(PlatformObj, nextTileSpawn, PlatformObj.rotation);
+        nextTileSpawn.z += 5;
+        StartCoroutine(spawnTile());
+    }
+}
