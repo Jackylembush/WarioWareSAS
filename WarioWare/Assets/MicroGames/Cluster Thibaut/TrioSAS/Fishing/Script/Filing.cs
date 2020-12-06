ba8b30e40e513899bedb1c1082f572ef58249ecc
Albane MATHEZ <a.mathez@gmail.com>
2020-12-06 17:56:59 +0100

    Recherche non-abouti

---
+++ WarioWare/Assets/MicroGames/Cluster Thibaut/TrioSAS/Fishing/Script/Filing.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Filing : MonoBehaviour
+{
+    public void OnValueChanged(float value)
+    {
+        Debug.Log("New Value" + value);
+    }
+}
